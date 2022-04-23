aae3921f7b66ae8c12ccad8dc037fb854a3d4a4d
OzerOzturk <ozerozturk1636@gmail.com>
2022-04-23 23:04:24 +0300

    delete unnecessary files at first stage.

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAe56Tipsj4Md0-mYApP-y2g2ono4Tp8YM",
+  authDomain: "amaznclone2022.firebaseapp.com",
+  projectId: "amaznclone2022",
+  storageBucket: "amaznclone2022.appspot.com",
+  messagingSenderId: "235045523680",
+  appId: "1:235045523680:web:c15f9c0cba00c931614085",
+};
