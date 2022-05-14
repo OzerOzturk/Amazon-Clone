eb0e8c37dcca2d8545d9f43d7b184008c41a681d
OzerOzturk <ozerozturk1636@gmail.com>
2022-05-14 16:27:31 +0300

    add user authentication.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Checkout from "./components/Checkout/Checkout";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //will only run once when the app component loads. its kind of if statement in react.
+    auth.onAuthStateChanged((authUser) => {
+      console.log("user is -->", authUser);
+
+      if (authUser) {
+        //the user just logged in or the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAe56Tipsj4Md0-mYApP-y2g2ono4Tp8YM",
   authDomain: "amaznclone2022.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "235045523680",
   appId: "1:235045523680:web:c15f9c0cba00c931614085",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
